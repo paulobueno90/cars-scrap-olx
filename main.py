18a66a02700c3a9c56a5dfea120de1161c07af09
Paulo Bueno <paulohmbueno@gmail.com>
2020-11-18 12:39:43 -0300

    Run Script commit

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 from fipe import *
 from spider import *
-from regression import *
+from clean_data import *
+#from regression import *
 
 
 # Take Fipe brand Keys and Model Keys
-get_models()
+#get_models()
 
-# Olx Crawler
-#search_ads(marca='honda', modelo='fit', estado='')
+# Olx Links Crawler
+#get_links('honda', 'fit', '')
+
+# Olx Ads Crawler
+#search_ads()
+
+# Clean Data
+clean_data()
 
 # Get Fipe Prices for each model and year
 #get_models_price()
 
 # Add fipe prices in Ads csv
 add_fipe2csv()
+
+# Variables Adjustment
+variables_adjustment()
+df_adjust()
+
 
